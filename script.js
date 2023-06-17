5c47ea5f7e30a054bc7bc4fd69df0e23237772d4
VitorDuraes <vitordo09@hotmail.com>
2023-06-17 19:07:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function escrevendoLetra() {
+  function ativaLetra(elemento) {
+    const arrTexto = elemento.innerHTML.split("");
+    elemento.innerHTML = "";
+    arrTexto.forEach((letra, i) => {
+      setTimeout(() => {
+        elemento.innerHTML += letra;
+      }, 75 * i);
+    });
+  }
+  const titulo = document.querySelector(".digitando");
+  ativaLetra(titulo);
+}
+
+const ativaMenu = document.querySelector(".fa-bars");
+const navMenu = document.querySelector("header .navegacao-primaria");
+
+ativaMenu.addEventListener("click", () => {
+  ativaMenu.classList.toggle("fa-x");
+  navMenu.classList.toggle("ativado");
+});
